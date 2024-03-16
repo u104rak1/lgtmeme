f400a0e0c500c6c0fbba915d99d9927b1221c17c
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2024-03-16 12:44:41 +0900

    feat: Set up Next.js and echo (#1)

---
+++ view/src/app/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginPage;
