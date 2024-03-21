4f1a9b5ffd2a4dab5d56165bd70e1ec514e46e85
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2024-03-21 20:12:34 +0900

    feat: Implement graceful shutdown (#26)

--- cmd/my_authn_authz/main.go
+++ cmd/my_authn_authz/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/ucho456job/my_authn_authz/config"
@@ -44,6 +48,21 @@ func main() {
 	e.HEAD(util.HEALTH_ENDPOINT, healthHandler.CheckHealth)
 	e.POST(util.LOGIN_ENDPOINT, loginHandler.Login)
 
+	// Graceful shutdown
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
 	port := ":" + os.Getenv("PORT")
-	e.Logger.Fatal(e.Start(port))
+	go func() {
+		if err := e.Start(port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
