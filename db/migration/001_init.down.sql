eeacdba6c70950185741b422a3882471255421ec
ucho456job <ucho456job@gmail.com>
2024-03-21 20:15:21 +0900

    fix: 001_init_down.sql

--- db/migration/001_init.down.sql
+++ db/migration/001_init.down.sql
@@ -1,3 +1,8 @@
 -- Down migration
 DROP TABLE IF EXISTS "users";
 DROP TABLE IF EXISTS "health_checks";
+DROP TABLE IF EXISTS "oauth_clients_application_types";
+DROP TABLE IF EXISTS "application_types";
+DROP TABLE IF EXISTS "oauth_clients_scopes";
+DROP TABLE IF EXISTS "scopes";
+DROP TABLE IF EXISTS "oauth_clients";
