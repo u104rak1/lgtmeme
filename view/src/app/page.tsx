962390b6f81e6f96016b0681de220bd2b3849dd9
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2024-03-30 00:46:45 +0900

    feat: Implement client auth handler (#45)
    * feat: Organize folder structure
    
    * feat: Implement client auth handler

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import Button from "@/components/atoms/Button/Button";
+import { ENDPOINTS } from "@/utils/constants";
+
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      <Button onClick={() => (window.location.href = ENDPOINTS.auth)}>
+        Auth
+      </Button>
     </div>
   );
 };
