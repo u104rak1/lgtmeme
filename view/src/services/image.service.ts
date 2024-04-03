11b00ed93fc0e08a4a798e5937e83a7222c8ce61
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2024-04-03 21:20:43 +0900

    feat: Implement image services for POST, PATCH, DELETE (#54)

--- view/src/services/image.service.ts
+++ view/src/services/image.service.ts
@@ -17,4 +17,50 @@ export class ImageService extends CommonService {
       return this.returnUnknownError();
     }
   }
+
+  async postImage(body: PostImageReqBody): Promise<PostImageResult> {
+    try {
+      const config = this.createConfig("POST", body);
+      const res = await fetch(CLIENT_ENDPOINTS.images, config);
+      if (!res.ok) {
+        const body: ErrRespBody = await res.json();
+        return { ...body, ok: false };
+      }
+      const resBody: PostImageRespBody = await res.json();
+      return { ...resBody, ok: true };
+    } catch {
+      return this.returnUnknownError();
+    }
+  }
+
+  async patchImage(
+    id: string,
+    body: PatchImageReqBody
+  ): Promise<PatchImageResult> {
+    try {
+      const config = this.createConfig("PATCH", body);
+      const res = await fetch(CLIENT_ENDPOINTS.images + "/" + id, config);
+      if (!res.ok) {
+        const body: ErrRespBody = await res.json();
+        return { ...body, ok: false };
+      }
+      return { ok: true };
+    } catch {
+      return this.returnUnknownError();
+    }
+  }
+
+  async deleteImage(id: string): Promise<DeleteImageResult> {
+    try {
+      const config = this.createConfig("DELETE");
+      const res = await fetch(CLIENT_ENDPOINTS.images + "/" + id, config);
+      if (!res.ok) {
+        const body: ErrRespBody = await res.json();
+        return { ...body, ok: false };
+      }
+      return { ok: true };
+    } catch {
+      return this.returnUnknownError();
+    }
+  }
 }
--- view/src/types/image.d.ts
+++ view/src/types/image.d.ts
@@ -3,6 +3,7 @@ type Image = {
   url: string;
 };
 
+/** GET */
 type GetImagesQuery = {
   page: number;
   keyword: string;
@@ -10,14 +11,66 @@ type GetImagesQuery = {
   favaoriteImageIds: string[];
   authCheck: boolean;
 };
-
 type GetImagesRespBody = {
   images: Image[];
 };
-
 type GetImagesSuccessResult = {
   images: Image[];
   ok: true;
 };
-
 type GetImagesResult = GetImagesSuccessResult | ErrResult;
+
+/** POST */
+type PostImageReqBody = {
+  image: string;
+  keyword: string;
+};
+type PostImageRespBody = {
+  imageUrl: string;
+};
+type PostImageSuccessResult = {
+  imageUrl: string;
+  ok: true;
+};
+type PostImageResult = PostImageSuccessResult | ErrResult;
+
+/** PATCH */
+type PatchRequestType = "used" | "report" | "confirmed";
+type PatchImageReqBody = {
+  type: PatchRequestType;
+};
+type PatchImageSuccessResult = {
+  ok: true;
+};
+type PatchImageResult = PatchImageSuccessResult | ErrResult;
+
+/** DELETE */
+type DeleteImageSuccessResult = {
+  ok: true;
+};
+type DeleteImageResult = DeleteImageSuccessResult | ErrResult;
+
+/** Image editor */
+type TextStyle = {
+  left: number;
+  top: number;
+  color: string;
+  fontSize: SizeMapKey;
+  width: number;
+  height: number;
+  lineHeight: string;
+  fontFamily: string;
+};
+type TextSizeSmall = 36;
+type TextSizeMedium = 60;
+type TextSizeLarge = 84;
+type SizeMapKey = TextSizeSmall | TextSizeMedium | TextSizeLarge;
+type SizeMap = Map<
+  SizeMapKey,
+  {
+    size: SizeMapKey;
+    width: number;
+    height: number;
+    diff: number;
+  }
+>;
