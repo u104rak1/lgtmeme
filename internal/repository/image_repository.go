31536868316b6d1c9161460dcb9fd3ac6a5c7f47
ucho456job <ucho456job@gmail.com>
2024-04-11 20:28:10 +0900

    fix

--- internal/repository/image_repository.go
+++ internal/repository/image_repository.go
@@ -48,7 +48,7 @@ func (r *imageRepository) Create(c echo.Context, id uuid.UUID, url, keyword stri
 }
 
 func (r *imageRepository) FindImages(c echo.Context, q dto.GetImagesQuery) (*[]model.Image, error) {
-	sqlQ := r.DB.Model(&model.Image{}).Select("id", "url")
+	sqlQ := r.DB.Debug().Model(&model.Image{})
 
 	if q.FavoriteImageIDs != "" {
 		favoriteImageIDs := strings.Split(q.FavoriteImageIDs, ",")
